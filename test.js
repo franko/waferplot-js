604643a93240b0e8f811713c240abccbb714b2ca
Francesco Abbate <francesco.bbt@gmail.com>
2015-11-30 18:09:52 +0100

    Adopt OrthographicCamera for the test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var wratio = window.innerWidth / window.innerHeight;
+var camera = new THREE.OrthographicCamera(-5 * wratio, 5 * wratio, 5, -5, -1, 100);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
